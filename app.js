af102572ea5fa5f7c3aeedea82622727c76c1ce8
Varshaaa25 <varshagowramma2003@gmail.com>
2024-01-23 21:09:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const sliderImgs = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.g"];
+let sliderImage = document.querySelector('.background-image');
+let sliderGrids = [...document.querySelectorAll('.grid-item')];
+
+let currentImage = 0;
+
+setInterval(() => {
+    changeSliderImage();
+}, 5000);
+
+const changeSliderImage = () => {
+    sliderGrids.map((gridItem, index) => {
+        setTimeout(() => {
+            gridItem.classList.remove('hide');
+
+            setTimeout(() => {
+
+                if(index == sliderGrids.length - 1){
+                    if(currentImage >= sliderImgs.length - 1){
+                        currentImage = 0;
+                    } else{
+                        currentImage++;
+                    }
+
+                    sliderImage.src = `img/${sliderImgs[currentImage]}`;
+
+                    sliderGrids.map((item, i) => {
+                        setTimeout(() => {
+                            item.classList.add('hide')
+                        }, i * 100);
+                    })
+
+                }
+
+            }, 100);
+
+        }, index * 100);
+    })
+}
